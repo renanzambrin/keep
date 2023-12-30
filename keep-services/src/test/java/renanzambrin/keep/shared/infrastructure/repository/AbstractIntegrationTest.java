860da4b38771e77a8e44fdc75b06a02f6b0e2c9a
Renan Zambrin <7773619+renanzambrin@users.noreply.github.com>
2023-12-30 13:03:29 +0000

    Add Health and Prometheus endpoints with Spring Actuator

--- keep-services/src/test/java/renanzambrin/keep/shared/infrastructure/repository/AbstractIntegrationTest.java
+++ keep-services/src/test/java/renanzambrin/keep/shared/infrastructure/repository/AbstractIntegrationTest.java
@@ -1,7 +1,10 @@
 package renanzambrin.keep.shared.infrastructure.repository;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -9,4 +12,14 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AbstractIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
 }
